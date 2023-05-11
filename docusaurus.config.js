4fb591b8e72934c88f06c72ffb6db987592570a2
peg <ameba23@systemli.org>
2023-05-11 10:56:02 +0200

    Rm blog link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -95,10 +95,10 @@ const config = {
           {
             title: 'More',
             items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
+              // {
+              //   label: 'Blog',
+              //   to: '/blog',
+              // },
               {
                 label: 'GitHub',
                 href: 'https://github.com/entropyxyz/entropy-core',
