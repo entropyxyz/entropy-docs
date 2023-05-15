2618071762e6ee8f3cf3cbb2eeb08a624d375d73
peg <ameba23@systemli.org>
2023-05-15 11:59:03 +0200

    Fix edit link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -41,7 +41,7 @@ const config = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: 'https://github.com/entropyxyz/entropy-docs',
+          editUrl: 'https://github.com/entropyxyz/entropy-docs/tree/master',
           remarkPlugins: [math],
           rehypePlugins: [katex],
         },
