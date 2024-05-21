ed72d76d6e3f3e474513ca1979d8975126848696
Johnny <9611008+johnnymatthews@users.noreply.github.com>
2024-05-21 13:35:40 -0300

    Creates a "Manage accounts" guide. (#60)
    * Fixes borked numbering in quickstart under Get Test Funds.
    
    * Makes the site look less like a Docusaurus site.
    
    * Adds Create an Address guide to sidebar.
    
    * Renames guide to "Manage accounts".

--- sidebars.js
+++ sidebars.js
@@ -32,11 +32,8 @@ export default {
       type: 'category',
       label: 'Guides',
       items: [
+        { type: 'doc', id: 'guides/manage-accounts', label: 'Manage accounts' },
         { type: 'doc', id: 'guides/get-test-funds', label: 'Get test funds' },
-    //    { type: 'doc', id: 'guides/create-a-wallet', label: 'Create a wallet' },
-    //    { type: 'doc', id: 'guides/register-an-address', label: 'Register an address' },
-    //    { type: 'doc', id: 'guides/deploy-a-program', label: 'Deploy a program' },
-    //    { type: 'doc', id: 'guides/query-the-chain', label: 'Query the chain' },
       ],
     },
     {
@@ -44,7 +41,6 @@ export default {
       label: 'Reference',
       items: [
         { type: 'doc', id: 'reference/cli', label: 'CLI' },
-        //{ type: 'doc', id: 'reference/sdk', label: 'SDK' },
       ],
     },
   ],
