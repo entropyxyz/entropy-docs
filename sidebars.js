87cbb1d4615c31e05aeae7ae185fa5bcbb20af01
johnnymatthews <9611008+johnnymatthews@users.noreply.github.com>
2024-06-04 23:35:01 -0300

    Adds register-an-account to sidebar.

--- sidebars.js
+++ sidebars.js
@@ -32,6 +32,7 @@ export default {
         { type: 'doc', id: 'guides/manage-accounts', label: 'Manage accounts' },
         { type: 'doc', id: 'guides/get-test-funds', label: 'Get test funds' },
         { type: 'doc', id: 'guides/transfer-funds', label: 'Transfer funds' },
+        { type: 'doc', id: 'guides/register-an-account', label: 'Register an account' },
         { type: 'doc', id: 'guides/use-the-explorer', label: 'Use the explorer' },
       ],
     },
