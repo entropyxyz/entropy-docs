5306bb7a1e27c57a6cea5b18287b45c2187a9fb0
Violet Parr <vi@entropy.xyz>
2024-06-18 14:07:52 -0400

    Issue #135: Suggest a few started OpenGraph API fields to use for docs.

---
+++ src/components/head.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Head from '@docusaurus/head'
+
+const EntropyOpenGraph = () => {
+  <Head>
+    <meta property="og:type" content="website">
+    <meta property="og:title" content="Entropy Documentation">
+    <meta property="og:url" content="https://docs.entropy.xyz/">
+    <meta property="og:image" content="https://docs.entropy.xyz/img/entropy-logo-black.png">
+    <meta property="og:description" content="Entropy is the Autonomous Agent Network, a Layer 1 blockchain protocol and for easy and secure decentralized signing infrastructure.">
+  </Head>
+}
