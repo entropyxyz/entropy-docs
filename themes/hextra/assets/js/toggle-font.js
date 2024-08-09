94bfba19b5f60548e4637a99880cd1ce6d9ee5c0
johnnymatthews <9611008+johnnymatthews@users.noreply.github.com>
2024-08-09 13:57:16 -0300

    Attempting to fix this weird static folder bug.

--- themes/hextra/assets/js/toggle-font.js
+++ themes/hextra/assets/js/toggle-font.js
@@ -1,43 +1,23 @@
 function toggleFont() {
     var contentElements = document.querySelectorAll('body');
-    var preElements = document.querySelectorAll('code');
-    var currentFont = localStorage.getItem('currentFont') || 'default';
+    var preElements = document.querySelectorAll('pre');
+    var currentFont = window.getComputedStyle(contentElements[0]).fontFamily;
 
-    if (currentFont === 'default') {
-        setDyslexicFonts();
-        localStorage.setItem('currentFont', 'dyslexic');
+    if (!currentFont.includes('Open-Dyslexic')) {
+        contentElements.forEach(function(element) {
+            element.style.fontFamily = 'Open-Dyslexic';
+        });
+        preElements.forEach(function(element) {
+            element.style.fontFamily = 'Open-Dyslexic-Mono';
+        });
+        console.log("Switch to Open-Dyslexic and Open-Dyslexic-Mono for pre tags.");
     } else {
-        setDefaultFonts();
-        localStorage.setItem('currentFont', 'default');
+        contentElements.forEach(function(element) {
+            element.style.fontFamily = 'sans-serif';
+        });
+        preElements.forEach(function(element) {
+            element.style.fontFamily = 'monospace';
+        });
+        console.log("Switch to sans-serif and monospace for pre tags.");
     }
 }
-
-function setDyslexicFonts() {
-    document.querySelectorAll('body').forEach(function(element) {
-        element.style.fontFamily = 'Open-Dyslexic';
-    });
-    document.querySelectorAll('code').forEach(function(element) {
-        element.style.fontFamily = 'Open-Dyslexic-Mono';
-    });
-    console.log("Switch to Open-Dyslexic and Open-Dyslexic-Mono for pre tags.");
-}
-
-function setDefaultFonts() {
-    document.querySelectorAll('body').forEach(function(element) {
-        element.style.fontFamily = 'sans-serif';
-    });
-    document.querySelectorAll('code').forEach(function(element) {
-        element.style.fontFamily = 'monospace';
-    });
-    console.log("Switch to sans-serif and monospace for pre tags.");
-}
-
-// Apply the saved font setting on page load
-document.addEventListener('DOMContentLoaded', function() {
-    var savedFont = localStorage.getItem('currentFont');
-    if (savedFont === 'dyslexic') {
-        setDyslexicFonts();
-    } else {
-        setDefaultFonts();
-    }
-});
