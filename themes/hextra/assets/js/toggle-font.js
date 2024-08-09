a427fdf31615d1740416a8125937e720ab6d77f9
johnnymatthews <9611008+johnnymatthews@users.noreply.github.com>
2024-08-09 13:32:23 -0300

    Toggle now works but doesn't persist.

--- themes/hextra/assets/js/toggle-font.js
+++ themes/hextra/assets/js/toggle-font.js
@@ -1,16 +1,23 @@
 function toggleFont() {
-    var contentElements = document.querySelectorAll('.content');
+    var contentElements = document.querySelectorAll('body');
+    var preElements = document.querySelectorAll('code');
     var currentFont = window.getComputedStyle(contentElements[0]).fontFamily;
 
     if (!currentFont.includes('Open-Dyslexic')) {
         contentElements.forEach(function(element) {
             element.style.fontFamily = 'Open-Dyslexic';
         });
-        console.log("Switch to Open-Dyslexic.");
+        preElements.forEach(function(element) {
+            element.style.fontFamily = 'Open-Dyslexic-Mono';
+        });
+        console.log("Switch to Open-Dyslexic and Open-Dyslexic-Mono for pre tags.");
     } else {
         contentElements.forEach(function(element) {
             element.style.fontFamily = 'sans-serif';
         });
-        console.log("Switch to sans-serif.");
+        preElements.forEach(function(element) {
+            element.style.fontFamily = 'monospace';
+        });
+        console.log("Switch to sans-serif and monospace for pre tags.");
     }
 }
