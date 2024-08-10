7caaa6afcc661c30b16df22bcbc91f079461bdaf
johnnymatthews <9611008+johnnymatthews@users.noreply.github.com>
2024-08-10 12:58:00 -0300

    Saves font choice to localStorage.

--- themes/hextra/assets/js/toggle-font.js
+++ themes/hextra/assets/js/toggle-font.js
@@ -1,23 +1,43 @@
-function toggleFont() {
-    var contentElements = document.querySelectorAll('body');
-    var preElements = document.querySelectorAll('pre');
-    var currentFont = window.getComputedStyle(contentElements[0]).fontFamily;
+var contentElements = document.querySelectorAll('body');
+var preElements = document.querySelectorAll('pre');
 
-    if (!currentFont.includes('Open-Dyslexic')) {
-        contentElements.forEach(function(element) {
-            element.style.fontFamily = 'Open-Dyslexic';
-        });
-        preElements.forEach(function(element) {
-            element.style.fontFamily = 'Open-Dyslexic-Mono';
-        });
-        console.log("Switch to Open-Dyslexic and Open-Dyslexic-Mono for pre tags.");
+function toggleFont() {
+    var currentFont = localStorage.getItem('currentFont') || 'default';
+
+    if (currentFont === 'default') {
+        setDyslexicFonts();
+        localStorage.setItem('currentFont', 'dyslexic');
     } else {
-        contentElements.forEach(function(element) {
-            element.style.fontFamily = 'sans-serif';
-        });
-        preElements.forEach(function(element) {
-            element.style.fontFamily = 'monospace';
-        });
-        console.log("Switch to sans-serif and monospace for pre tags.");
+        setDefaultFonts();
+        localStorage.setItem('currentFont', 'default');
     }
 }
+
+function setDyslexicFonts() {
+    contentElements.forEach(function(element) {
+        element.style.fontFamily = 'Open-Dyslexic';
+    });
+    preElements.forEach(function(element) {
+        element.style.fontFamily = 'Open-Dyslexic-Mono';
+    });
+    console.log("Switch to Open-Dyslexic and Open-Dyslexic-Mono for pre tags.");
+}
+
+function setDefaultFonts() {
+    contentElements.forEach(function(element) {
+        element.style.fontFamily = 'sans-serif';
+    });
+    preElements.forEach(function(element) {
+        element.style.fontFamily = 'monospace';
+    });
+    console.log("Switch to sans-serif and monospace for pre tags.");
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    var savedFont = localStorage.getItem('currentFont');
+    if (savedFont === 'dyslexic') {
+        setDyslexicFonts();
+    } else {
+        setDefaultFonts();
+    }
+});
